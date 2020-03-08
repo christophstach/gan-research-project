f8fd545de87fc0c1008f4b9a3232dcef7daae4d4
Christoph Stach <christoph.stach@gmail.com>
2020-03-08 12:23:53 +0800

    added logger to hparams

--- gans/wgan/train_wgan.py
+++ gans/wgan/train_wgan.py
@@ -10,18 +10,21 @@ from gans.wgan import WGAN
 def main(hparams):
     model = WGAN(hparams)
 
-    use_logger = False
-
-    if use_logger:
-        comet_logger = CometLogger(
+    if hparams.logger == "none":
+        logger = None
+    elif hparams.logger == "comet.ml":
+        logger = CometLogger(
             api_key=os.environ["COMET_KEY"],
             workspace=os.environ["COMET_WORKSPACE"],  # Optional
             project_name="research-project-gan",  # Optional
             rest_api_key=os.environ["COMET_REST_KEY"],  # Optional
             experiment_name="Wasserstein GAN"  # Optional
         )
+    elif hparams.logger == "tensorboard":
+        # not implemented yet
+        raise NotImplementedError("Tensorboard logger is not implemented yet")
     else:
-        comet_logger = None
+        raise ValueError("Must specific a logger")
 
     trainer = Trainer(
         min_epochs=hparams.min_epochs,
@@ -30,16 +33,17 @@ def main(hparams):
         nb_gpu_nodes=hparams.nodes,
         accumulate_grad_batches=hparams.accumulate_grad_batches,
         early_stop_callback=False,
-        logger=comet_logger if use_logger else False
+        logger=logger
     )
 
     trainer.fit(model)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = ArgumentParser(add_help=False)
-    parser.add_argument('--gpus', type=str)
-    parser.add_argument('--nodes', type=int, default=1)
+    parser.add_argument("--logger", type=str, choices=["none", "comet.ml", "tensorboard"])
+    parser.add_argument("--gpus", type=str)
+    parser.add_argument("--nodes", type=int, default=1)
 
     parser = WGAN.add_model_specific_args(parser)
 
