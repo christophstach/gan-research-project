3c5eff7b1166653961b7305ee34a2659a7407791
Christoph Stach <christoph.stach@gmail.com>
2020-03-11 14:02:16 +0800

    wrong dataset

--- run_experiments.sh
+++ run_experiments.sh
@@ -2,5 +2,5 @@ git pull
 clear
 
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 500 --dataset mnist
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 500 --dataset fhasion_mnist
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 500 --dataset fashion_mnist
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 10000 --dataset cifar10
