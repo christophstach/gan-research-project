0f0442eb903f82d0cc6e2bfd40482274c10f8557
Christoph Stach <christoph.stach@gmail.com>
2020-03-11 13:39:59 +0800

    more epochs for cifar10

--- run_experiments.sh
+++ run_experiments.sh
@@ -3,4 +3,4 @@ clear
 
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 500 --dataset mnist
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 500 --dataset fhasion_mnist
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 1000 --dataset cifar10
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 256 --logger comet.ml --max-epochs 10000 --dataset cifar10
