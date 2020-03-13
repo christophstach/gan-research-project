5717d7e02bdb53776acdafc4c5c44734ab37d568
Christoph Stach <christoph.stach@gmail.com>
2020-03-14 02:17:28 +0800

    wip

--- gans/wgan_gp/wgan_gp.py
+++ gans/wgan_gp/wgan_gp.py
@@ -132,7 +132,7 @@ class WGANGP(pl.LightningModule):
     def on_epoch_end(self):
         if self.logger:
             num_images = self.y_size if self.y_size > 0 else 6
-            noise = torch.randn(num_images, self.noise_size, 1, 1)
+            noise = torch.randn(num_images, self.noise_size)
             y = torch.tensor(range(num_images))
 
             if self.on_gpu:
