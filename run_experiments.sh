f13a8b8bf2d8640ba6de87aca46f5b06e68c133d
Christoph Stach <christoph.stach@gmail.com>
2020-03-14 02:07:19 +0800

    wip

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,3 +4,4 @@ clear
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 200 --dataset mnist
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 200 --dataset fashion_mnist
 python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 200 --dataset cifar10
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 2000 --dataset cifar10
