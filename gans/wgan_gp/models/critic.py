79de4dd22824ac754be1e46a763c4a1565dacb7b
Christoph Stach <christoph.stach@gmail.com>
2020-03-15 20:53:08 +0800

    wip

--- gans/wgan_gp/models/critic.py
+++ gans/wgan_gp/models/critic.py
@@ -16,15 +16,15 @@ class Critic(nn.Module):
             nn.LeakyReLU(self.leaky_relu_slope, inplace=True),
             # state size. (self.image_size) x 32 x 32
             nn.Conv2d(self.image_size, self.image_size * 2, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.image_size * 2),
+            # nn.BatchNorm2d(self.image_size * 2),
             nn.LeakyReLU(self.leaky_relu_slope, inplace=True),
             # state size. (self.image_size*2) x 16 x 16
             nn.Conv2d(self.image_size * 2, self.image_size * 4, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.image_size * 4),
+            # nn.BatchNorm2d(self.image_size * 4),
             nn.LeakyReLU(self.leaky_relu_slope, inplace=True),
             # state size. (self.image_size*4) x 8 x 8
             nn.Conv2d(self.image_size * 4, self.image_size * 8, 4, 2, 1, bias=False),
-            nn.BatchNorm2d(self.image_size * 8),
+            # nn.BatchNorm2d(self.image_size * 8),
             nn.LeakyReLU(self.leaky_relu_slope, inplace=True),
             # state size. (self.image_size*8) x 4 x 4
             nn.Conv2d(self.image_size * 8, 1, 4, 1, 0, bias=False),
