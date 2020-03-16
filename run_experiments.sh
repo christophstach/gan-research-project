f71fdbee254603585e25441141b1a0f747dd931c
Christoph Stach <christoph.stach@gmail.com>
2020-03-16 14:40:07 +0800

    wip

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,7 +1,7 @@
 git pull
 clear
 
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 500 --dataset mnist --loss-type wgan-gp1
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 500 --dataset mnist --loss-type wgan-gp2
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 500 --dataset fashion_mnist --loss-type wgan-gp1
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 500 --dataset cifar10 --loss-type wgan-gp1
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 20 --dataset mnist --loss-type wgan-gp1
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 20 --dataset mnist --loss-type wgan-gp2
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 20 --dataset fashion_mnist --loss-type wgan-gp1
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 20 --dataset cifar10 --loss-type wgan-gp1
