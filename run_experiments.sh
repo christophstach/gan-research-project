961491d4697e66d98de650c8f246743f84cbcf2c
Christoph Stach <christoph.stach@gmail.com>
2020-03-18 00:11:16 +0800

    wip

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,4 +1,4 @@
 git pull
 clear
 
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 5000 --dataset cifar10 --loss-type wgan-gp1
+python gans/wgan_gp/train_wgan_gp.py --gpus 0 --batch-size 64 --logger comet.ml --max-epochs 5000 --dataset cifar10 --loss-type wgan-gp1
