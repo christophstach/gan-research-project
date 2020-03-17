1a7df3417af6b95fe8bfeedcf273b65b5f205daf
Christoph Stach <christoph.stach@gmail.com>
2020-03-17 16:09:09 +0800

    wip

--- gans/wgan_gp/wgan_gp.py
+++ gans/wgan_gp/wgan_gp.py
@@ -281,7 +281,7 @@ class WGANGP(pl.LightningModule):
         pretrain_group = parser.add_argument_group("Pretrain")
         pretrain_group.add_argument("-pe", "--pretrain-enabled", type=bool, default=True, help="Enables pretraining of the critic with an classification layer on the real data")
         pretrain_group.add_argument("-pmine", "--pretrain-min-epochs", type=float, default=1, help="Minimum pretrain epochs")
-        pretrain_group.add_argument("-pmaxe", "--pretrain-max-epochs", type=float, default=10, help="Maximum pretrain epochs")
+        pretrain_group.add_argument("-pmaxe", "--pretrain-max-epochs", type=float, default=25, help="Maximum pretrain epochs")
         pretrain_group.add_argument("-pagb", "--pretrain-accumulate-grad-batches", type=float, default=1, help="Number of gradient batches to accumulate during pretraining")
 
         generator_group = parser.add_argument_group("Generator")
--- run_experiments.sh
+++ run_experiments.sh
@@ -1,5 +1,4 @@
 git pull
 clear
 
-python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 500 --dataset fashion_mnist --loss-type wgan-gp1
-# python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 50 --dataset mnist --loss-type wgan-gp2
+python gans/wgan_gp/train_wgan_gp.py --gpus 1 --batch-size 64 --logger comet.ml --max-epochs 5000 --dataset cifar10 --loss-type wgan-gp1
