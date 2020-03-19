eda0c564c25e6dc3bcbecff1da80d76cbb38cc98
Christoph Stach <christoph.stach@gmail.com>
2020-03-19 13:43:58 +0800

    wip

--- gans/wgan_gp/models/generator.py
+++ gans/wgan_gp/models/generator.py
@@ -15,19 +15,19 @@ class Generator(pl.LightningModule):
             nn.ConvTranspose2d(self.hparams.noise_size + self.hparams.y_embedding_size, self.hparams.image_size * 4, kernel_size=4, stride=1, padding=0),
             nn.InstanceNorm2d(self.hparams.image_size * 4),
             # nn.PReLU(),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
 
             # Conv2dPixelShuffle(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=3, upscale_factor=2),
             nn.ConvTranspose2d(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=4, stride=2, padding=1),
             nn.InstanceNorm2d(self.hparams.image_size * 2),
             # nn.PReLU(),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
 
             # Conv2dPixelShuffle(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=3, upscale_factor=2),
             nn.ConvTranspose2d(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=4, stride=2, padding=1),
             nn.InstanceNorm2d(self.hparams.image_size),
             # nn.PReLU(),
-            nn.ReLU(),
+            nn.ReLU(inplace=True),
 
             # Conv2dPixelShuffle(self.hparams.image_size, self.hparams.image_channels, kernel_size=3, upscale_factor=2),
             nn.ConvTranspose2d(self.hparams.image_size, self.hparams.image_channels, kernel_size=4, stride=2, padding=1),
