2be35c196e33cd7520409c3c2e83d8da6caa24c5
Christoph Stach <christoph.stach@gmail.com>
2020-03-19 15:34:06 +0800

    wip

--- gans/wgan_gp/models/generator.py
+++ gans/wgan_gp/models/generator.py
@@ -2,6 +2,8 @@ import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 
+from ...building_blocks import Conv2dPixelShuffle
+
 
 class Generator(pl.LightningModule):
     def __init__(self, hparams):
@@ -11,29 +13,29 @@ class Generator(pl.LightningModule):
 
         self.main = nn.Sequential(
             # input is Z, going into a convolution
-            # Conv2dPixelShuffle(self.hparams.noise_size + self.hparams.y_embedding_size, self.hparams.image_size * 4, kernel_size=4, upscale_factor=2),
-            nn.ConvTranspose2d(self.hparams.noise_size + self.hparams.y_embedding_size, self.hparams.image_size * 4, kernel_size=4, stride=1, padding=0),
+            Conv2dPixelShuffle(self.hparams.noise_size + self.hparams.y_embedding_size, self.hparams.image_size * 4, kernel_size=4, upscale_factor=2),
+            # nn.ConvTranspose2d(self.hparams.noise_size + self.hparams.y_embedding_size, self.hparams.image_size * 4, kernel_size=4, stride=1, padding=0),
             # nn.InstanceNorm2d(self.hparams.image_size * 4),
             nn.BatchNorm2d(self.hparams.image_size * 4),
             # nn.PReLU(),
             nn.PReLU(),
 
-            # Conv2dPixelShuffle(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=3, upscale_factor=2),
-            nn.ConvTranspose2d(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=4, stride=2, padding=1),
+            Conv2dPixelShuffle(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=3, upscale_factor=2),
+            # nn.ConvTranspose2d(self.hparams.image_size * 4, self.hparams.image_size * 2, kernel_size=4, stride=2, padding=1),
             # nn.InstanceNorm2d(self.hparams.image_size * 2),
             nn.BatchNorm2d(self.hparams.image_size * 2),
             # nn.PReLU(),
             nn.PReLU(),
 
-            # Conv2dPixelShuffle(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=3, upscale_factor=2),
-            nn.ConvTranspose2d(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=4, stride=2, padding=1),
+            Conv2dPixelShuffle(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=3, upscale_factor=2),
+            # nn.ConvTranspose2d(self.hparams.image_size * 2, self.hparams.image_size, kernel_size=4, stride=2, padding=1),
             # nn.InstanceNorm2d(self.hparams.image_size),
             nn.BatchNorm2d(self.hparams.image_size),
             # nn.PReLU(),
             nn.PReLU(),
 
-            # Conv2dPixelShuffle(self.hparams.image_size, self.hparams.image_channels, kernel_size=3, upscale_factor=2),
-            nn.ConvTranspose2d(self.hparams.image_size, self.hparams.image_channels, kernel_size=4, stride=2, padding=1),
+            Conv2dPixelShuffle(self.hparams.image_size, self.hparams.image_channels, kernel_size=3, upscale_factor=2),
+            # nn.ConvTranspose2d(self.hparams.image_size, self.hparams.image_channels, kernel_size=4, stride=2, padding=1),
             nn.Tanh()
         )
 
