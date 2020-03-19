adf29a67328c97a44dba1059f02e32f1fa4b996a
Christoph Stach <christoph.stach@gmail.com>
2020-03-19 14:57:01 +0800

    wip

--- gans/wgan_gp/models/critic.py
+++ gans/wgan_gp/models/critic.py
@@ -18,17 +18,17 @@ class Critic(pl.LightningModule):
         self.features = nn.Sequential(
             nn.Conv2d(self.hparams.image_channels, self.hparams.image_size, kernel_size=4, stride=2, padding=1),
             # nn.LayerNorm([self.hparams.image_size, int(self.hparams.image_size / 2), int(self.hparams.image_size / 2)]),
-            nn.PReLU(self.hparams.image_size),
+            nn.PReLU(),
             # nn.LeakyReLU(0.2, inplace=True),
 
             nn.Conv2d(self.hparams.image_size, self.hparams.image_size * 2, kernel_size=4, stride=2, padding=1),
             # nn.LayerNorm([self.hparams.image_size * 2, int(self.hparams.image_size / 4), int(self.hparams.image_size / 4)]),
-            nn.PReLU(self.hparams.image_size * 2),
+            nn.PReLU(),
             # nn.LeakyReLU(0.2, inplace=True),
 
             nn.Conv2d(self.hparams.image_size * 2, self.hparams.image_size * 4, kernel_size=4, stride=2, padding=1),
             # nn.LayerNorm([self.hparams.image_size * 4, int(self.hparams.image_size / 8), int(self.hparams.image_size / 8)]),
-            nn.PReLU(self.hparams.image_size * 4)
+            nn.PReLU()
             # nn.LeakyReLU(0.2, inplace=True)
         )
 
