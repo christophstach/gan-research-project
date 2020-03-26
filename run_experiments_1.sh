7a535d00883c6f556b29c37d602cea3f4c24d6c6
Christoph Stach <christoph.stach@gmail.com>
2020-03-26 13:50:27 +0800

    wip

--- run_experiments_1.sh
+++ run_experiments_1.sh
@@ -1,4 +1,4 @@
 git pull
 clear
 
-python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --strategy wgan-0-gp
+python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset mnist --strategy wgan-1-gp
--- run_experiments_3.sh
+++ run_experiments_3.sh
@@ -1,4 +1,4 @@
 git pull
 clear
 
-python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --strategy wgan-lp
+python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --strategy wgan-1-gp
