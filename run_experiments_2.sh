9c98276d1cd4c7657f170d2f24dcd0ece973079a
Christoph Stach <christoph.stach@gmail.com>
2020-03-30 14:56:57 +0800

    wip

--- run_experiments_2.sh
+++ run_experiments_2.sh
@@ -1,4 +1,4 @@
 git pull
 clear
 
-python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --loss-strategy fashion_mnist --gradient-penalty-strategy lp --gradient-penalty-term 0.1
+python gans/train_gan.py --gpus 0 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset fashion_mnist --loss-strategy wgan --gradient-penalty-strategy lp --gradient-penalty-term 0.1
