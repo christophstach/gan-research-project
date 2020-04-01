5a8d9df577a0341a20fc053d86c7d5bf299a2dde
Christoph Stach <christoph.stach@gmail.com>
2020-04-01 09:24:07 +0800

    wip

--- run_experiments_2.sh
+++ run_experiments_2.sh
@@ -1,4 +1,4 @@
 git pull
 clear
 
-python gans/train_gan.py --gpus 1 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --loss-strategy wagan --gradient-penalty-strategy 1-gp --gradient-penalty-weight 10 --consistency-term-weight 2
+python gans/train_gan.py --gpus 1 --dataloader-num-workers 10 --batch-size 64 --logger wandb --max-epochs 5000 --dataset cifar10 --loss-strategy wgan --gradient-penalty-strategy 1-gp --gradient-penalty-weight 10 --consistency-term-weight 2
