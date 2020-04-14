e77e87e88c6261a50c27bd01d0bb1a17ea30faed
Christoph Stach <christoph.stach@gmail.com>
2020-04-14 23:40:13 +0800

    wip

--- run_experiments_2.sh
+++ run_experiments_2.sh
@@ -1,1 +1,1 @@
-python gans/train_gan.py --gpus 1 --dataloader-num-workers 10 --batch-size 32 --logger wandb --max-epochs 10000 --dataset lsun --loss-strategy ra-hinge --gradient-penalty-strategy none --image-size 256
+python gans/train_gan.py --gpus 1 --dataloader-num-workers 10 --batch-size 32 --logger wandb --max-epochs 10000 --dataset lsun --loss-strategy ra-hinge --gradient-penalty-strategy 0-gp --image-size 256
