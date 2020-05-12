0993d477c1dcbe2ec38047d488b35891ca2c2419
Christoph Stach <christoph.stach@gmail.com>
2020-05-12 17:26:01 +0800

    wip

--- run_experiments_2.sh
+++ run_experiments_2.sh
@@ -3,7 +3,7 @@ python gans/train_gan.py \
   --max-epochs 10000 \
   --dataset celeba_hq \
   --dataloader-num-workers 10 \
-  --batch-size 64 \
+  --batch-size 32 \
   --image-size 512 \
   --logger wandb \
   --loss-strategy ra-sgan \
