7dc1f1248d69e28412b0c529094fe04335d383b5
Christoph Stach <christoph.stach@gmail.com>
2020-06-01 09:43:42 +0800

    wip

--- run_experiments_1.sh
+++ run_experiments_1.sh
@@ -4,6 +4,8 @@ python gans/train_gan.py \
   --dataset celeba_hq \
   --dataloader-num-workers 10 \
   --exponential-filter-multipliers \
+  --generator-filters 4 \
+  --discriminator-filters 4 \
   --batch-size 32 \
   --image-size 256 \
   --noise-size 256 \
