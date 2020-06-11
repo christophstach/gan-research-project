60c15cdd0a7694980aa06227fed81db62c2896d1
Christoph Stach <christoph.stach@gmail.com>
2020-06-11 09:30:42 +0800

    fix gpu bug

--- gans/regularization.py
+++ gans/regularization.py
@@ -9,7 +9,7 @@ def orthogonal_regularization(model, device):
             if 'bias' not in name:
                 param_flat = param.view(param.shape[0], -1)
                 sym = torch.mm(param_flat, torch.t(param_flat))
-                sym -= torch.eye(param_flat.shape[0])
+                sym -= torch.eye(param_flat.shape[0], device=device)
                 orth_loss = orth_loss + (reg * sym.abs().sum())
 
     return orth_loss
