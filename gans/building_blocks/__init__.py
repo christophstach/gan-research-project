c91d5e86f1829a3df44ee96b92c889423f5c4ed2
Christoph Stach <christoph.stach@gmail.com>
2020-06-18 12:58:19 +0800

    wip

--- gans/building_blocks/__init__.py
+++ gans/building_blocks/__init__.py
@@ -2,4 +2,4 @@ from .attention import SelfAttention2d
 from .minibatch_std_dev import MinibatchStdDev
 from .pixel_norm import PixelNorm
 from .convolution import Conv2d, ConvTranspose2d, SubPixelConv2d, _ConvTranspose2d, _Conv2d
-from .switch_norm import SwitchNorm2d
+from .switchable_norm import SwitchNorm2d
