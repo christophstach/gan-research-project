6618fde44bfa1ba605696e07ff867c1f5168f1b0
Christoph Stach <christoph.stach@gmail.com>
2020-07-03 22:44:53 +0800

    wip

--- gans/regularization.py
+++ gans/regularization.py
@@ -13,3 +13,32 @@ def orthogonal_regularization(model, device):
                 orth_loss = orth_loss + (reg * sym.abs().sum())
 
     return orth_loss
+
+# Apply modified ortho reg to a model
+# This function is an optimized version that directly computes the gradient,
+# instead of computing and then differentiating the loss.
+def ortho(model, strength=1e-4, blacklist=[]):
+  with torch.no_grad():
+    for param in model.parameters():
+      # Only apply this to parameters with at least 2 axes, and not in the blacklist
+      if len(param.shape) < 2 or any([param is item for item in blacklist]):
+        continue
+      w = param.view(param.shape[0], -1)
+      grad = (2 * torch.mm(torch.mm(w, w.t()) 
+              * (1. - torch.eye(w.shape[0], device=w.device)), w))
+      param.grad.data += strength * grad.view(param.shape)
+
+
+# Default ortho reg
+# This function is an optimized version that directly computes the gradient,
+# instead of computing and then differentiating the loss.
+def default_ortho(model, strength=1e-4, blacklist=[]):
+  with torch.no_grad():
+    for param in model.parameters():
+      # Only apply this to parameters with at least 2 axes & not in blacklist
+      if len(param.shape) < 2 or param in blacklist:
+        continue
+      w = param.view(param.shape[0], -1)
+      grad = (2 * torch.mm(torch.mm(w, w.t()) 
+               - torch.eye(w.shape[0], device=w.device), w))
+      param.grad.data += strength * grad.view(param.shape)
